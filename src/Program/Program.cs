94d41c30fe10dd9e70cdfd6740141d45736c4e49
JuanMMaidana <juan.maidana@correo.ucu.edu.uy>
2021-10-04 12:36:51 -0300

    commiting

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -11,10 +11,10 @@ namespace Program
             book.AddSpell(new SpellOne());
             book.AddSpell(new SpellOne());
 
-            Wizard gandalf = new Wizard("Gandalf");
+            IMagicCharacter gandalf = new Wizard("Gandalf");
             gandalf.AddItem(book);
 
-            Dwarf gimli = new Dwarf("Gimli");
+            Character gimli = new Dwarf("Gimli");
 
             Console.WriteLine($"Gimli has ❤️ {gimli.Health}");
             Console.WriteLine($"Gandalf attacks Gimli with ⚔️ {gandalf.AttackValue}");
